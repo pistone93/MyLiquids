8976ab7e333a91e45d435cae0953f8bbef565b86
Luigi <luigi.isidori@yahoo.it>
2023-04-17 18:28:13 +0200

    refactoring and added some new element

--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Menu } from '../../shared/model/menu.model';
+import { newArray } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  menuVoice: Menu[] = [];
 
-  constructor() { }
+  constructor() {
+    this.generateMenu();
+   }
 
   ngOnInit(): void {
   }
 
+  private generateMenu(): void{
+    this.menuVoice.push({link: '/',name: 'Home'});
+    this.menuVoice.push({link: '/user',name: 'Utente'});
+    this.menuVoice.push({link: '/my-liquids',name: 'I miei liquidi'});
+    this.menuVoice.push({link: '/calculator',name: 'Strumenti'});
+    console.log(this.menuVoice);
+  }
 }
---
+++ src/app/shared/model/menu.model.ts
@@ -0,0 +1,4 @@
+export interface Menu {
+    link: string,
+    name: string
+  }
