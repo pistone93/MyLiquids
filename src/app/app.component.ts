0450e2ec028e2c0c7f7440ed87cea84feb5af975
Luigi <luigi.isidori@yahoo.it>
2023-04-17 15:01:31 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'MyLiquids';
+  isSidebarOpened = false;
+
+  ngOnInit(): void {
+    this.toggleSidebar();
+  }
+
+  toggleSidebar() {
+    this.isSidebarOpened = !this.isSidebarOpened;
+    const sidebar = document.querySelector('.sidebar');
+    const container = document.querySelector('.sidebar-container');
+    sidebar.classList.toggle('open');
+    if (this.isSidebarOpened) {
+      container.classList.add('sidebar-opened');
+      container.classList.remove('sidebar-closed');
+    } else {
+      container.classList.add('sidebar-closed');
+      container.classList.remove('sidebar-opened');
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './core/home/home.component';
+import { SidebarComponent } from './core/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
